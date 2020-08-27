e1b564c0663d802d5ef029d86cc2289f1f1f510b
Carlos Fletes <carlos.fletes@marvelsa.com>
2020-08-27 14:15:53 -0500

    1.0.0

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Search on website by compatible',
+    'version': '12.0',
+    'sequence': 0,
+    'category': 'Website',
+    "author": "Marvelsa",
+    "website": "http://www.marvelsa.com",
+
+    'description': """
+                Product Global Search
+        """,
+    'depends': ['base', 'product', 'sale', 'website_sale', 'website_mail', 'sale_management', 'marvelfields'],
+    'data': [
+
+    ],
+
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ controllers/main.py
@@ -0,0 +1,40 @@
+from odoo import http
+from odoo.http import request
+from odoo.addons.http_routing.models.ir_http import slug
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo.addons.website_sale.controllers.main import TableCompute
+from odoo.addons.website_sale.controllers.main import QueryURL
+
+
+class WebsiteSale(WebsiteSale):
+
+    def _get_search_domain(self, search, category, attrib_values):
+        print("entro aqui")
+        domain = request.website.sale_product_domain()
+        if search:
+            for srch in search.split(" "):
+                domain += [
+                    '|', '|', '|', '|', ('name', 'ilike', srch), ('description', 'ilike', srch),
+                    ('description_sale', 'ilike', srch),
+                    ('product_variant_ids.default_code', 'ilike', srch), ('compatible_ids', 'ilike', srch)]
+
+        if category:
+            domain += [('public_categ_ids', 'child_of', int(category))]
+
+        if attrib_values:
+            attrib = None
+            ids = []
+            for value in attrib_values:
+                if not attrib:
+                    attrib = value[0]
+                    ids.append(value[1])
+                elif value[0] == attrib:
+                    ids.append(value[1])
+                else:
+                    domain += [('attribute_line_ids.value_ids', 'in', ids)]
+                    attrib = value[0]
+                    ids = [value[1]]
+            if attrib:
+                domain += [('attribute_line_ids.value_ids', 'in', ids)]
+
+        return domain
